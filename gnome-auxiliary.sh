fced384b0e9912af2e46e1c48824015aaa4718b2
BlvckBytes <blvckbytes@gmail.com>
2025-11-03 22:11:50 +0100

    disabled auto-raise

--- gnome-auxiliary.sh
+++ gnome-auxiliary.sh
@@ -14,7 +14,7 @@ gsettings set org.gnome.shell.keybindings screenshot "['']"
 gsettings set org.gnome.shell.keybindings screenshot-window "['']"
 
 gsettings set org.gnome.desktop.wm.preferences focus-mode 'sloppy'
-gsettings set org.gnome.desktop.wm.preferences auto-raise 'true'
+gsettings set org.gnome.desktop.wm.preferences auto-raise 'false'
 gsettings set org.gnome.desktop.wm.preferences auto-raise-delay 0
 gsettings set org.gnome.mutter overlay-key ''
 gsettings set org.gnome.shell.keybindings toggle-overview "['<Super>space']"
