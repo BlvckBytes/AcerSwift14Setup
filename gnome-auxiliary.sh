ab006579825c3f31bbd3352b3f08aff7cc2be395
BlvckBytes <blvckbytes@gmail.com>
2025-11-04 00:28:43 +0100

    unbound conflicting shortcut; added keybind for run-dialog

--- gnome-auxiliary.sh
+++ gnome-auxiliary.sh
@@ -5,7 +5,7 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys logout '[]'
 gsettings set org.gnome.shell.keybindings focus-active-notification '[]'
 gsettings set org.gnome.shell.keybindings toggle-message-tray '[]'
 gsettings set org.gnome.shell.keybindings toggle-application-view '[]'
-gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog '[]'
+gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog "['<Super>r']"
 gsettings set org.gnome.mutter.wayland.keybindings restore-shortcuts '[]'
 
 # The additional "AI"-key on my keyboard - let's get some use out of it! :)
@@ -24,3 +24,6 @@ gsettings set org.gnome.desktop.interface enable-hot-corners "false"
 gsettings set org.gnome.desktop.interface show-battery-percentage "true"
 gsettings set org.gnome.mutter edge-tiling "false"
 gsettings set org.gnome.mutter dynamic-workspaces "false"
+
+# By default, it's <Super>p, which would shadow show/hide top-panel
+gsettings set org.gnome.mutter.keybindings switch-monitor "[]"
